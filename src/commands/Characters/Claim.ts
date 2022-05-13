02cb3ebb47e3278fcebfe7cbe3af9efa396110dd
synbee <97864273+synbee@users.noreply.github.com>
2022-05-13 23:02:08 +0530

    Update Claim.ts

--- src/commands/Characters/Claim.ts
+++ src/commands/Characters/Claim.ts
@@ -56,7 +56,7 @@ export default class Command extends BaseCommand {
       { $set: { "charaResponse.claimable": false } }
     );
     return void M.reply(
-      `🎊 You have claimed *${data.name}* from *${data.source}*.`
+      `🎉 You have successfully claimed *${data.name}* from *${data.source} It has been stored in your deck*.`
     );
   };
 }
