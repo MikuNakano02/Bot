511041c9d60175d78fdf95c1b4ccfc7c3d6d070d
xVambOx <105160137+xVambo@users.noreply.github.com>
2022-05-14 10:24:04 +0530

    Update leadboard.ts

--- src/commands/Fun/leadboard.ts
+++ src/commands/Fun/leadboard.ts
@@ -46,7 +46,7 @@ export default class Command extends BaseCommand {
         ? this.client.contacts[users.jid].short
         : `@${users.jid.split("@")[0]}`;
 
-      const text = `*#${i}*\n*♦️Username* : *${username}*\n*🎊 Exp*: *${users.Xp}*\n*🏦 Bank*: *${users.bank}*\n*🪙 Gold* : *${users.wallet}*\n♦️ *Pokemons: ${users.pokemons.length}*\n🎗 *Characters: ${users.gallery.length}*\n 🍀 *Quiz Points: ${users.quizPoints}*\n`;
+      const text = `*#${i}*\n*♦️Username* : *${username}*\n*🎊 Role: ${role}*\n*🏦 Bank*: *${users.bank}*\n*🪙 Gold* : *${users.wallet}*\n♦️ *Pokemons: ${users.pokemons.length}*\n🎗 *Characters: ${users.gallery.length}*\n 🍀 *Quiz Points: ${users.quizPoints}*\n`;
       message.push(text);
     }
     console.log(message);
