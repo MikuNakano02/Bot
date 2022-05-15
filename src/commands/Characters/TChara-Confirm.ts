9de19f87f53b3c591d5b0cf720f9e67ed4c46d8e
synbee <97864273+synbee@users.noreply.github.com>
2022-05-15 10:41:17 +0530

    SKSKSKSKSK

--- src/commands/Characters/TChara-Confirm.ts
+++ src/commands/Characters/TChara-Confirm.ts
@@ -107,10 +107,8 @@ export default class Command extends BaseCommand {
         { $set: { "charaTrade.ongoing": false } }
       );
       return void M.reply(
-        `Trade Completed! ${data.for.name} (From ${data.for.source}) -----> @${
-          data.startedBy.split("@")[0]
-        }\n${data.offer.name} (From ${data.offer.source}) -----> @${
-          user.split("@")[0]
+        `*🎊 Trade Complete*\n\n🎗️ @${data.startedBy.split("@")[0] <=${data.for.name} - (From ${data.for.source})
+        }\n🎗️ @${user.split("@")[0] <=${data.offer.name} - (From ${data.offer.source})
         }`,
         MessageType.text,
         undefined,
