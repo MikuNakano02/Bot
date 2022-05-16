5f5dbc0ec4ac50f3e379c4546a56cba0af6afb60
synbee <97864273+synbee@users.noreply.github.com>
2022-05-16 21:25:32 +0530

    Update leadboard.ts

--- src/commands/Fun/leadboard.ts
+++ src/commands/Fun/leadboard.ts
@@ -29,9 +29,11 @@ export default class Command extends BaseCommand {
 
     // M.reply('total user heuhue :'+indexes.toString())
     const message = ["*👑CELESTIAL GLOBAL LEADERBOARD👑*\nUsers :" + indexes.toString() + ""];
+    const mention = [];
     let i = 0;
     for (const users of lb) {
       console.log(users.jid, "number");
+      mention.push(users.jid);
       i++;
       console.log(i);
       const username = this.client.contacts[users.jid]
@@ -42,7 +44,7 @@ export default class Command extends BaseCommand {
         ? this.client.contacts[users.jid].name
         : this.client.contacts[users.jid]
         ? this.client.contacts[users.jid].short
-        : `@${users.jid.split("@")[0]}`;
+        : `${users.jid.split("@")[0]}`;
 
       const text = `*#${i}*\n*🧧 Username* : *${username}*\n*👛 Wallet* : *${users.wallet}*\n*🏦 Bank*: *${users.bank}*\n*🎖️ Exp*: *${users.Xp}*\n💻 *Pokemons: ${users.pokemons.length}*\n⭐ *Characters: ${users.gallery.length}*\n📊 *Quiz Points: ${users.quizPoints}*\n`;
       message.push(text);
