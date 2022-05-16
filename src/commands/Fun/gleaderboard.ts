ca9589de61e88ef1a2bb36c4bb5337be230d32a1
xVambOx <105160137+xVambo@users.noreply.github.com>
2022-05-16 22:49:55 +0530

    Update gleaderboard.ts

--- src/commands/Fun/gleaderboard.ts
+++ src/commands/Fun/gleaderboard.ts
@@ -43,7 +43,7 @@ export default class Command extends BaseCommand {
         ? this.client.contacts[users.jid].name
         : this.client.contacts[users.jid]
         ? this.client.contacts[users.jid].short
-        : `${users.jid.split[0]}`;
+        : `${users.jid.split}`;
 
       let text = `*#${i}*\n*🧧 Username* : ${username}\n*👛 Wallet* : ${users.wallet}\n*🏦 Bank* : ${users.bank}\n*🎖️ Exp*: ${users.Xp}\n`;
       message.push(text);
