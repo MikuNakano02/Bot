2ad9226fe55373afff02cff9a5f3e8cf58116bf2
synbee <97864273+synbee@users.noreply.github.com>
2022-05-16 21:58:34 +0530

    Update leadboard.ts

--- src/commands/Fun/leadboard.ts
+++ src/commands/Fun/leadboard.ts
@@ -44,7 +44,7 @@ export default class Command extends BaseCommand {
         ? this.client.contacts[users.jid].name
         : this.client.contacts[users.jid]
         ? this.client.contacts[users.jid].short
-        : `${users.jid.split("@")[0]}`;
+        : `@${users.jid.split("@")[0]}`;
 
       const text = `*#${i}*\n*🧧 Username* : *${username}*\n*👛 Wallet* : *${users.wallet}*\n*🏦 Bank*: *${users.bank}*\n*🎖️ Exp*: *${users.Xp}*\n💻 *Pokemons: ${users.pokemons.length}*\n⭐ *Characters: ${users.gallery.length}*\n📊 *Quiz Points: ${users.quizPoints}*\n`;
       message.push(text);
@@ -54,8 +54,7 @@ export default class Command extends BaseCommand {
       await request.buffer("https://www.linkpicture.com/q/wp3470722-space-wallpaper-hd-purple-01.jpeg"),
       MessageType.image,
       undefined,
-      mention,
-      message.join("\n\n")
+      mention, ")
     );
   };
 }
