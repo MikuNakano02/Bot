5076e022bc8f3752a78d55c39f91323d2603d3ea
xVambOx <105160137+xVambo@users.noreply.github.com>
2022-05-19 19:09:56 +0530

    Update Getgold.ts

--- src/commands/Dev/Getgold.ts
+++ src/commands/Dev/Getgold.ts
@@ -1,3 +1,4 @@
+
 import { MessageType } from "@adiwajshing/baileys";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
@@ -18,14 +19,24 @@ export default class Command extends BaseCommand {
     });
   }
 
-  run = async (M: ISimplifiedMessage): Promise<void> => {
-   if (!joined)
+  run = async (
+    M: ISimplifiedMessage,
+    { joined }: IParsedArgs
+  ): Promise<void> => {
+    if (!joined)
       return void (await M.reply(`Please provide the amount of gold to add.`));
-    const term: any = joined.split(" ")[0]; 
+    const term: any = joined.split(" ")[0];
     const user = M.sender.jid;
-    }
-  {  {
-    await this.client.addGold(user, term);
-    }
-    return void M.reply(`🎉Added ${term} gold to wallet.`);
+    if (isNaN(term)) return void M.reply(`Well... It should be a number.`);
+  await this.client.DB.user
+      .exec(async (err, res) => {
+        if (err) return void M.reply(`...`);
+        for (let i = 0; i < res.length; i++) {
+          await this.client.addGold(user, term);
+        }
+        return void M.reply(
+          `🟩 *Added ${term} gold to ${res.length} users wallet.*`
+        );
+      });
+  };
 }
